43c05e39e1f47aa9d7e9f4e00d5b290da0c3d394
kousuke.tamogami <okasuke.0714@gmial.com>
2022-01-25 16:20:46 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', 'MicropostsController@index');
 
 Route::resource('tasks', 'TasksController');
 
@@ -24,4 +24,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
